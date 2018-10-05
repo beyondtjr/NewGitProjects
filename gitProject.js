7956e72fa149207cb02ff6b47104bf556afd1a92
byeondtjr <beyondtjr@gmail.com>
2018-10-05 11:29:51 -0700

    Adding notes to initial commit

--- gitProject.js
+++ gitProject.js
@@ -1,1 +1,5 @@
 console.log("Started a new project with an initial commit.");
+
+console.log("test");
+
+// Testing to see what happens when I make a change and commit to a file that is not "Initialize read me"
