007d16c2831746988782100e38631196c4e1162c
byeondtjr <beyondtjr@gmail.com>
2018-10-05 11:32:15 -0700

    Testing note additions

--- gitProject.js
+++ gitProject.js
@@ -3,3 +3,5 @@ console.log("Started a new project with an initial commit.");
 console.log("test");
 
 // Testing to see what happens when I make a change and commit to a file that is not "Initialize read me"
+
+// Testing notes 2. Does it update when adding notes? 
